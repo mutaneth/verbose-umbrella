84b398158a1012af880d54253044e03ba26c4301
lamsnakesgit <cybvvow@gmail.com>
2019-07-27 20:46:49 +0300

    include libft.h

--- Makefile
+++ Makefile
@@ -1,46 +1,14 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    Makefile                                           :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: hfalmer <hfalmer@student.42.fr>            +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2019/07/19 03:42:58 by hfalmer           #+#    #+#              #
-#    Updated: 2019/07/24 20:55:44 by hfalmer          ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
-
-.PHONY: all, $(NAME), norm, clean, fclean, re
-
-#****************************************************************************#
-#					SETTINGS FOR CONCLUSION IN CONSOLE 						 #
-#****************************************************************************#
-
-RED = \033[31m
-GREEN = \033[32m
-YELLOW = \033[33m
-BLUE = \033[34m
-PINK = \033[35m
-AQUA = \033[36m
-GREY = \033[37m
-UNDERLINE = \033[4m
-NORMAL = \033[0m
-
-#****************************************************************************#
-#									FILLIT 									 #
-#****************************************************************************#
-
 NAME = fillit
-
 SRC_PATH = ./source/
 OBJ_PATH = ./objects/
 INC_PATH = ./includes/
+INC_LIB_H_PATH = ./includes/libft/includes
 LIB_PATH = ./includes/libft/
 
 SRC = $(addprefix $(SRC_PATH), $(SRC_FILES))
 OBJ = $(addprefix $(OBJ_PATH), $(OBJ_NAME))
 INC = $(addprefix -I, $(INC_PATH))
+INC_LH_PATH = $(addprefix -I, $(INC_LIB_H_PATH))
 
 SRC_NAME =	main.c			\
 			validate.c		\
@@ -51,28 +19,14 @@ SRC_NAME =	main.c			\
 OBJ_NAME = $(SRC_NAME:.c=.o)
 INC_NAME = fillit.h
 
-#****************************************************************************#
-#									ALL 									 #
-#****************************************************************************#
-
 all: $(NAME)
 
 $(NAME): $(OBJ)
 	make lib_refresh
 	gcc -o $(NAME) $(OBJ) -lm -L $(LIB_PATH) -lft
-	echo "$(PINK)"###"\t\t           $(PINK)💥  FILLIT READY! 💥\t\t\t$(PINK)     "###""
-	echo "$(AQUA)***************$(AQUA)***************\
-$(AQUA)***************$(AQUA)***************$(AQUA)************"
-	echo "\t\t$(PINK)       🦄 🌈 CAN YOU FEEL IT?🦄 🌈"
-
 $(OBJ_PATH)%.o: $(SRC_PATH)%.c
 	mkdir -p $(OBJ_PATH)
-	gcc -Wall -Werror -Wextra $(INC) -o $@ -c $<
-
-#****************************************************************************#
-#									OTHER			 						 #
-#****************************************************************************#
-
+	gcc -Wall -Werror -Wextra $(INC) $(INC_LH_PATH) -o $@ -c $<
 lib_refresh:
 	make -C $(LIB_PATH)
 
--- fillit.h
+++ fillit.h
@@ -3,35 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   fillit.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hfalmer <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: hfalmer <hfalmer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/17 03:02:14 by hfalmer           #+#    #+#             */
-/*   Updated: 2019/07/14 20:38:19 by ddratini         ###   ########.fr       */
+/*   Updated: 2019/07/27 20:27:02 by ddratini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILLIT_H
+
 # define FILLIT_H
-#include <fcntl.h>
-#include <stdio.h>/////
-#include "../fillit/libft/libft.h"
-#include <math.h>
-/*
-//gnl
-t_lsti				*ft_lstinew(void *cont, size_t cont_size, int index);
-t_lsti				*ft_lstimap(t_lsti *lst, t_lsti *(*f)(t_lsti *elem));
-void				ft_lstidel(t_lsti **alst, void (*del)(void *, size_t));
-void				ft_lstidelone(t_lsti **alst, void (*del)(void *, size_t));
-void				ft_lstiiter(t_lsti *lst, void (*f)(t_lsti *elem));
-void				ft_lstiadd(t_lsti **alst, t_lsti *new);
-typedef struct		s_lsti
-{
-	void			*content;
-	size_t			content_size;
-	int				index;
-	struct s_lsti	*next;
-}					t_lsti;
-*/
+# include <fcntl.h>
+# include "./libft.h"
+
 typedef struct		s_fgr
 {
 	int				fgr_int;
@@ -40,31 +24,36 @@ typedef struct		s_fgr
 	struct s_fgr	*next;
 }					t_fgr;
 
-# define FGR1 0000 0000 29 0 401 281 57 27 153
+char				*fgrl_chck(int i, int gr, char *tfl, char *fgrl);
+int					six(char **map, t_fgr *fgr, int y, int x);
+void				print_map(char **map);
+void				dot(char **map, t_fgr *fi);
+int					place(char **map, t_fgr *fgr, int y, int x);
+int					putin(char **map, t_fgr *fgr, int y, int x);
+int					twenty_one(char **map, t_fgr *fgr, int y, int x);
+int					rcrs(char ***minmap, t_fgr *fgrlst, int y, int x);
+t_fgr				*al_chck(char **line, t_fgr *fgr);
 
-int					six(char **map, t_fgr *fgr, int y, int x);//correct placement for non corn fgrs
-unsigned int		rev_bit(unsigned char octet);
-char				*ft_fgr_line(int fd);// malloc??
-t_fgr				*fgr_new(char fgr_chr, char *line);/* creates new node in t_fgr. chr starts with 65 and then iterates to 90*/
-t_fgr				*fgr_lst(char i, char *line, t_fgr *fgr); /*     */
-t_fgr				*after_line(int fd, char **line);//, char *LINE);/* \n between figures && no symbols after \0 EOF && EOF after figures */
-int					fgrl_xtra_01(char *fgr_line);/* checks if this line is valid and change symbols to 1 or 0. if it's not val, just frees*/
-int					ft_fgr_int(char *fgrl);/* takes fgrl and turns it to int using bits logic after fgrl_val*/
+char				*ft_fgr_line(int fd);
+t_fgr				*fgr_new(char fgr_chr, char *line);
+void				fgr_push_back(t_fgr **begin_list, char *data, char i);
+t_fgr				*after_line(int fd, char **line);
+int					fgrl_xtra_01(char *fgr_line);
+int					ft_fgr_int(char *fgrl);
 int					int_check(t_fgr *fgr);
 t_fgr				*mega_fgr_val(int fd);
-int					putin(char **map, t_fgr *fgr, int y, int x);/* puts 1 fgr in map */
-int					fgr_count(t_fgr *fgrlst);/* just count fgrs*/
-char				**mapc(int flg);/* creates the minimal start map*/
-void				map_free(char **map, int fgc);//t_fgr *fgrlst);// huita
-char				**map_increaser(t_fgr *fgrlst, char **map, int flg);// flg = iterrator
-int     			check_n(int fd);
-int 				place(char **map, t_fgr *fgr, int y, int x);//int s);//(int f);
-int					four_check(char **map, t_fgr *fgr, int x, int y, int s);
-int					check_input_count(char *blc); //check for wrong chars and extra #
-int					rcrs(char ***min_map, t_fgr *fgrlst);//recursion /
-void				free_fgr(t_fgr **fgr);
-void				print_map(char **map);
+void				free_fgr(t_fgr *fgr);
 
-//forbidden funcs in header>?
+int					check_n(int fd);
+
+int					ft_sqrt(int nb);
+int					fgr_count(t_fgr *fgrlist);
+char				**mapc(int flg);
+void				map_free(char **map, int fgc);
+int					rcrs(char ***minmap, t_fgr *fgrlst, int y, int xxx);
+
+int					read_max(int fd);
+int					st(char **av);
+void				build_f(t_fgr *r);
 
 #endif
